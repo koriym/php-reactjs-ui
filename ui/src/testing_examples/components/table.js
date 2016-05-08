a1b9be8ee265ccddbd86fe9156ac29b37965bfd2
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-05-08 14:34:11 +0900

    copy table.js

--- ui/src/testing_examples/components/table.js
+++ ui/src/testing_examples/components/table.js
@@ -1,18 +1,19 @@
-/**!
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-global.Table = React.createClass({
+Table = React.createClass({
   render: function () {
+    var rows = this.props.data.map(function (row) {
+      var cells = row.map(function(cell) {
+        return <td>{cell}</td>;
+      });
+
+      return <tr>{cells}</tr>;
+    });
+
     return (
-      <div>
-        Hello World !
-      </div>
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
     );
   }
 });
+
+module.exports = Table;
