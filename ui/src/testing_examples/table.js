724ac9483b043fadd21c25cbd2f6c78debab40f9
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-05-08 14:28:36 +0900

    declare as global component

--- ui/src/testing_examples/table.js
+++ ui/src/testing_examples/table.js
@@ -6,9 +6,8 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
-var React = require('react');
 
-var Table = React.createClass({
+global.Table = React.createClass({
   render: function () {
     var rows = this.props.data.map(function (row) {
       var cells = row.map(function(cell) {
