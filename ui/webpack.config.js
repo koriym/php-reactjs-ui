5860ed64eb29bc72e974e80a66a15105cda388d5
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-06-07 01:23:03 +0900

    update HMR v3

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         exclude: /(node_modules)/
       },
       {
