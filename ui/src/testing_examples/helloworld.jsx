5b207fee46ab1acf8555ba76a649e371b05306dd
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-09-21 16:44:52 +0900

    accept hmr

--- ui/src/testing_examples/helloworld.jsx
+++ ui/src/testing_examples/helloworld.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/Helloworld.js';
 
 ReactDOM.render(
-  <HelloWorld name="Worldo" />,
+  <HelloWorld name="World" />,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept();
+}
